980a3bb15e87866c9fcbc8a8967d41d60826fb0d
sumaeta <brenosumaeta33@gmail.com>
2021-05-24 22:10:39 -0400

    First commit

---
+++ src/main/java/com/demo/mvc/MovicApplication.java
@@ -0,0 +1,13 @@
+package com.demo.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/mvc/MovicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
