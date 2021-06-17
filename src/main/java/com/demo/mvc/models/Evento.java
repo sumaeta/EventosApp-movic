9529376ec06b4c2aa876329a148e03bb0a65e3b9
sumaeta <brenosumaeta33@gmail.com>
2021-06-16 22:04:16 -0300

    Conectando ao banco de dados

--- src/main/java/com/demo/mvc/models/Evento.java
+++ src/main/java/com/demo/mvc/models/Evento.java
@@ -3,6 +3,8 @@ package com.demo.mvc.models;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -14,6 +16,7 @@ public class Evento implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String local;
